6d68f2125ad0f610aa2a26658c5ba4220a25b35b
Whitewolf <whitewolf.mot185@gmail.com>
2020-03-04 16:27:37 +0300

    some changes

---
+++ cmake-build-debug/CMakeFiles/maks.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/maks.dir/maks.c.o"
+  "maks"
+  "maks.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/maks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
