d2dfd6fe840a8c575b72625ca8bff996948538b0
Whitewolf <whitewolf.mot185@gmail.com>
2020-03-04 19:04:03 +0300

    thinking on parsing

--- start.sh
+++ start.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 echo "Script started"
-if [[ "$@" -lt 3 ]]
-then
-  echo "Missing arguments"
-  exit 1
-fi
-file=$"$@"
-let "num=$@-2"
+
+let "num=$#-2"
 #exicutable keys start
 key_help=0
 key_add=0
@@ -15,22 +10,29 @@ key_show=0
 key_show_alldb=0
 key_del_alldb=0
 key_create=0
-
+key_MakeMyTask=0
 #exicutable keys end
 #__________________________
+echo $#
 for (( i=1; i <= $num; i++ ))
 do
-  case $1 in
-    "-h" | "--help" ) key_help=1 ;;
+  if [ "$1" = "-*" ]; then
+
+  fi
+  case "$1" in
+    "-h" | "--help" ) key_help=1;;
     "-a" | "--add" ) key_add=1 ;;
     "-q" | "--request" ) key_show=1 ;;
     "-sa" | "--show_all" ) key_show_alldb=1 ;;
     "-da" | "--delete_all" ) key_del_alldb=1 ;;
     "-d" | "--delete" ) key_del=1 ;;
     "-c" | "--create" ) key_create=1 ;;
+    "-m" | "--make" ) key_MakeMyTask=1 ;;
   esac
   shift
 done
+
+
 
 param="$1"
 file_path="$2"
@@ -58,6 +60,12 @@ then
   exit 0
 fi
 
+if [[ "$#" -lt 2 ]]
+then
+  echo "Missing arguments"
+  exit 1
+fi
+
 if [[ "$key_show" -eq 1 ]]
 then
   ./cmake-build-debug/kp6 q || exit 1
@@ -65,4 +73,12 @@ fi
 
 if [[ "$key_add" -eq 1 ]]; then
     ./cmake-build-debug/kp6 a || exit 1
+fi
+
+if [ "$key_del" -eq 1 ]; then
+    ./cmake-build-debug/kp6 d || exit 1
+fi
+
+if [ "$key_MakeMyTask" -eq 1 ]; then
+    ./cmake-build-debug/kp6 m || exit 1
 fi
