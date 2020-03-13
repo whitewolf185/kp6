4eb7b0a4c610a2c957ed5d326c7edd652e0409f4
Whitewolf <whitewolf.mot185@gmail.com>
2020-03-13 11:17:14 +0300

    added request

--- kp6.c
+++ kp6.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
-enum boolean{true=1, false=0};
-typedef enum boolean bool;
+#include <math.h>
+#define log10(n) floor(log(n))
+typedef enum boolean{true=1, false=0} bool;
 
 
-struct data {
+int str_to_int(char x[], int size){
+    int ans=0;
+    int pos=1;
+    for(int i=size-1; i>=0; i--){
+        if(x[i]<'0' || x[i]>'9') return -1;
+        ans+=pos*(x[i]-'0');
+        pos*=10;
+    }
+    return ans;
+}
+
+bool str_to_bool(char x[]){
+    if(x == "false"){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+typedef struct data {
     int ID;
     char lastname[32];
     char init [2];
@@ -13,17 +34,43 @@ struct data {
     char time[5];
     bool got_change;
     bool got_children;
-};
-typedef struct data data;
+} data;
+
 
 void clear(char x[], int size){
     for(int i=0; i<size; i++) x[i]=0;
 }
-//TODO надо продумать счетчик. скорее всего это будет просто просмотр последнего ID, но это как-то долго
+
 int main(int num, char* args[]) {
     //request
     if (args[1][0] == 'q'){
+        FILE *file=NULL;
+        file = fopen(args[3], "rb");
+        data man;
+        while(fread(&man, sizeof(data), 1, file)){
+            if(man.ID == str_to_int(args[2],log10(man.ID)) || man.destination == args[2] || man.got_change == str_to_bool(args[2]) ||
+            man.lastname == args[2] || man.init == args[2] || man.number == str_to_int(args[2],2) || man.wight == str_to_int(args[2],2) ||
+            man.time == args[2] || man.got_children == str_to_bool(args[2])) {// сумки и вес должны быть двух знаков, наличие детей обозначается true или false
+                printf("%d %s %s %d %d %s %s ",man.ID , man.lastname, man.init, man.number, man.wight, man.destination, man.time);
+                if(man.got_change){
+                    printf("true ");
+                }
+                else{
+                    printf("false ");
+                }
 
+                if(man.got_children){
+                    printf("true");
+                }
+                else{
+                    printf("false");
+                }
+                fclose(file);
+                return 0;
+            }
+        }
+        printf("No such man in DB");
+        return 2;
     }
 
     //add
